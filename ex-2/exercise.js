b0fa4ca1020a6278a38be9b98a7252cf0dbea5a0
liban <botanlee515@gmail.com>
2021-03-02 12:32:28 +0100

    Done

--- ex-2/exercise.js
+++ ex-2/exercise.js
@@ -1,3 +1,15 @@
 function getAttributes() {
-
+    var u = document.getElementById("pwcdr").href;
+    alert('The value of the href attribute of the link is : '+u);
+    var v = document.getElementById("pwcdr").hreflang;   
+    alert('The value of the hreflang attribute of the link is : '+v);
+    var w = document.getElementById("pwcdr").rel; 
+     alert('The value of the rel attribute of the link is : '+w);
+    var x = document.getElementById("pwcdr").target; 
+     alert('The value of the taget attribute of the link is : '+x);
+    var y = document.getElementById("pwcdr").type; 
+     alert('The value of the type attribute of the link is : '+y);
+     var js = 'Value = more JS power!'
+     alert(js)
 }
+
